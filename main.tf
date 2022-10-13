c661547bbc89cb06e2c8862b734923f50728bf83
lokesh7 <115445012+lokesh679@users.noreply.github.com>
2022-10-13 10:46:22 +0530

    formatting code

--- main.tf
+++ main.tf
@@ -1,1 +1,20 @@
 data "azurerm_resource_group" "rg"{
+  name                = var.resource_group_name
+}
+
+resource "azure_app_service_plan" "appln"{
+  name                = var.azure_app_service_plan
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azure_app_service" "webapp" {
+  name                = var.name
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+}
