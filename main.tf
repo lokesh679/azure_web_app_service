18698a770313513fc3c0b11b343a5eb671a58118
lokesh7 <115445012+lokesh679@users.noreply.github.com>
2022-10-14 11:19:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "3.26.0"
+      version = "~>2.07.0"
     }
   }
 }
 
-
+provider "azurerm" {
+  features {}
+}
 
 
 data "azurerm_resource_group" "rg"{
